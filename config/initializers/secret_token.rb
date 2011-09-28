7e7eb0a2cf7156dc8dd629d5705f016e0eb0e1b8
headleyra <headleyra@yahoo.com>
2011-09-28 15:11:15 +0100

    Bundle the gem file: Rails, RSpec, ...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkedinApiTest::Application.config.secret_token = '53ee23ce5f3387cca118baa905fe8f064a6d411cf1eaab397a2308ba0972830df6f47d129a769641eda9b0cb92138df356e2d90620419b674cc04a7e6872c013'
